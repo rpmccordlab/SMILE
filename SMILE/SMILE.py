0c0ba5f1ac5bb807cf9a6dfa3209b54b02832492
Yang Xu <35667575+ImXman@users.noreply.github.com>
2022-07-04 18:51:02 -0400

    Update SMILE.py

--- SMILE/SMILE.py
+++ SMILE/SMILE.py
@@ -168,23 +168,17 @@ def PairedSMILE_trainer(X_a, X_b, model, batch_size = 512, num_epoch=5,
         
 ##-----------------------------------------------------------------------------
 ##Updates 05/09/2022
-def ReferenceSMILE_trainer(X_a_paired, X_b_paired,X_a_unpaired,X_b_unpaired, model, batch_size = 512, num_epoch=5, 
-                        f_temp = 0.1, p_temp = 1.0):
+def ReferenceSMILE_trainer(X_a_paired, X_b_paired,X_a_unpaired,X_b_unpaired, model, 
+                           batch_size = 512, pretrain_epoch=10,train_epoch=1000,
+                           f_temp = 0.1):
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     f_con = ContrastiveLoss(batch_size = batch_size,temperature = f_temp)
-    p_con = ContrastiveLoss(batch_size = model.clf_out,temperature = p_temp)
-    opt = torch.optim.SGD(model.parameters(),lr=0.01, momentum=0.9,weight_decay=5e-4)
+    opt = torch.optim.Adam(model.parameters(),lr=0.001,weight_decay=5e-4)
     
-    for k in range(num_epoch):
-        
+    for k in range(pretrain_epoch):
+        model.train()
         model.to(device)
-        n = X_a_paired.shape[0]
-        r = np.random.permutation(n)
-        X_train_a = X_a_paired[r,:]
-        X_tensor_A=torch.tensor(X_train_a).float()
-        X_train_b = X_b_paired[r,:]
-        X_tensor_B=torch.tensor(X_train_b).float()
         
         n2 = X_a_unpaired.shape[0]
         r = np.random.permutation(n2)
@@ -195,39 +189,54 @@ def ReferenceSMILE_trainer(X_a_paired, X_b_paired,X_a_unpaired,X_b_unpaired, mod
         r = np.random.permutation(n2)
         X_train_b_u = X_b_unpaired[r,:]
         X_tensor_Bu=torch.tensor(X_train_b_u).float()
-        n= min(X_a_paired.shape[0],X_a_unpaired.shape[0],X_b_unpaired.shape[0])
+        n= min(X_a_unpaired.shape[0],X_b_unpaired.shape[0])
         
         losses = 0
         
         for j in range(n//batch_size):
-            inputs_a = X_tensor_A[j*batch_size:(j+1)*batch_size,:].to(device)
             inputs_au = X_tensor_Au[j*batch_size:(j+1)*batch_size,:].to(device)
             inputs_au2 = inputs_au + torch.normal(0,1,inputs_au.shape).to(device)
             inputs_au = inputs_au + torch.normal(0,1,inputs_au.shape).to(device)
             
-            inputs_b = X_tensor_B[j*batch_size:(j+1)*batch_size,:].to(device)
             inputs_bu = X_tensor_Bu[j*batch_size:(j+1)*batch_size,:].to(device)
             inputs_bu2 = inputs_bu + torch.normal(0,1,inputs_bu.shape).to(device)
             inputs_bu = inputs_bu + torch.normal(0,1,inputs_bu.shape).to(device)
             
-            feas,o,nfeas,no = model(inputs_a,inputs_b)
-            feasu,ou,nfeasu,nou = model(inputs_au,inputs_bu)
-            feasu2,ou2,nfeasu2,nou2 = model(inputs_au2,inputs_bu2)
-            f_a = model.encoder_a(inputs_a)
-            f_b = model.encoder_b(inputs_b)
+            feaA,feaB = model(inputs_au,inputs_bu)
+            feaA2,feaB2 = model(inputs_au2,inputs_bu2)
         
-            mse_loss = torch.nn.MSELoss()(f_a,f_b)#*2.0
-            fea_mi = f_con(feas,nfeas)*5.0+f_con(feasu,feasu2)+f_con(nfeasu,nfeasu2)#*5.0 or 4.0
-            p_mi = p_con(o.T,no.T)*5.0+p_con(ou.T,ou2.T)+p_con(nou.T,nou2.T)#*5.0 or 4.0
-        
-            loss = fea_mi + p_mi + mse_loss
+            loss = f_con(feaA,feaA2)+f_con(feaB,feaB2)
+            
             opt.zero_grad()
             loss.backward()
             opt.step()
         
             losses += loss.data.tolist()
-        print("Total loss: "+str(round(losses,4)))
-        gc.collect()  
+        gc.collect()
+        
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.train()
+    model.to(device)
+    
+    x_a = torch.tensor(X_a_paired).float()
+    x_b = torch.tensor(X_b_paired).float()
+    x_a = x_a.to(device)
+    x_b = x_b.to(device)
+    
+    opt = torch.optim.Adam(model.parameters(),lr=0.001,weight_decay=5e-4)
+    
+    for e in range(train_epoch):
+        
+        A,B = model(x_a,x_b)
+        
+        loss = (CosineSimilarity(B,A)+CosineSimilarity(A,B))/2
+        
+        #Backward
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        
+        gc.collect()
 
 ##-----------------------------------------------------------------------------
 ##Updates 06/30/2022
